b29741e5a47e96f158faaedf00332a0390f0086e
eric1234463 <eric1234463@gmail.com>
2017-10-30 21:35:13 +0800

    add fxml

--- src/kiosk/Ticket.java
+++ src/kiosk/Ticket.java
@@ -2,36 +2,35 @@ package kiosk;
 
 public class Ticket {
     private String action;
-    private String clientId;
-    private String nPersons;
-    private String ticketNo;
-    private String tableNo;
+    private Integer clientId;
+    private Integer nPersons;
+    private Integer ticketNo;
+    private Integer tableNo;
     public Ticket(String message) {
         String[] ObjectMapper = message.split(" ");
         this.action = ObjectMapper[0];
         switch (this.action) {
             case "TicketReq:":
-                this.clientId = ObjectMapper[1];
-                this.nPersons = ObjectMapper[2];
-
+                this.clientId = Integer.parseInt(ObjectMapper[1]);
+                this.nPersons = Integer.parseInt(ObjectMapper[2]);
                 break;
             case "TicketRep:":
-                this.clientId = ObjectMapper[1];
-                this.nPersons = ObjectMapper[2];
-                this.ticketNo = ObjectMapper[3];
+                this.clientId = Integer.parseInt(ObjectMapper[1]);
+                this.nPersons = Integer.parseInt(ObjectMapper[2]);
+                this.ticketNo = Integer.parseInt(ObjectMapper[3]);
                 break;
             case "TicketCall:":
-                this.ticketNo = ObjectMapper[1];
-                this.tableNo = ObjectMapper[2];
+                this.ticketNo = Integer.parseInt(ObjectMapper[1]);
+                this.tableNo = Integer.parseInt(ObjectMapper[2]);
                 break;
             case "TicketAck:":
-                this.ticketNo = ObjectMapper[1];
-                this.tableNo = ObjectMapper[2];
-                this.nPersons = ObjectMapper[3];
+                this.ticketNo = Integer.parseInt(ObjectMapper[1]);
+                this.tableNo = Integer.parseInt(ObjectMapper[2]);
+                this.nPersons = Integer.parseInt(ObjectMapper[3]);
                 break;
             case "TableAssign:":
-                this.ticketNo = ObjectMapper[1];
-                this.tableNo = ObjectMapper[2];
+                this.ticketNo = Integer.parseInt(ObjectMapper[1]);
+                this.tableNo = Integer.parseInt(ObjectMapper[2]);
                 break;
         }
     }
@@ -40,39 +39,19 @@ public class Ticket {
         return action;
     }
 
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public String getClientId() {
+    public Integer getClientId() {
         return clientId;
     }
 
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getnPersons() {
+    public Integer getnPersons() {
         return nPersons;
     }
 
-    public void setnPersons(String nPersons) {
-        this.nPersons = nPersons;
-    }
-
-    public String getTicketNo() {
+    public Integer getTicketNo() {
         return ticketNo;
     }
 
-    public void setTicketNo(String ticketNo) {
-        this.ticketNo = ticketNo;
-    }
-
-    public String getTableNo() {
+    public Integer getTableNo() {
         return tableNo;
-    }
-
-    public void setTableNo(String tableNo) {
-        this.tableNo = tableNo;
     }
 }
