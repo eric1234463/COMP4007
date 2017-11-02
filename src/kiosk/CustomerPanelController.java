b1ce09cd05ec1c19c685119cfba7354b152585f8
Man Kin Hung Bear <man_kinhung@hotmail.com.hk>
2017-11-03 03:06:18 +0800

    add id to buttons, label

--- src/kiosk/CustomerPanelController.java
+++ src/kiosk/CustomerPanelController.java
@@ -1,16 +1,30 @@
 package kiosk;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CustomerPanelController {
+public class CustomerPanelController implements Initializable {
     private Scene scene;
 
-    public void numberButtonPressed(ActionEvent actionEvent){
-        System.out.println(actionEvent.getID());
+    @FXML
+    private Label label_display;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
     }
-    public void setScene(Scene scene){
+
+    public void numberButtonPressed(ActionEvent actionEvent) {
+        label_display.setText(((Button) actionEvent.getSource()).getId());
+    }
+
+    public void setScene(Scene scene) {
         this.scene = scene;
 
     }
