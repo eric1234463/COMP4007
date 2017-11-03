c513882972c58c997268984b511abee1acf1d254
Man Kin Hung Bear <man_kinhung@hotmail.com.hk>
2017-11-03 14:31:55 +0800

    finish customer panel ui

--- src/kiosk/CustomerPanelController.java
+++ src/kiosk/CustomerPanelController.java
@@ -20,12 +20,31 @@ public class CustomerPanelController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
     }
 
-    public void numberButtonPressed(ActionEvent actionEvent) {
-        label_display.setText(((Button) actionEvent.getSource()).getId());
-    }
 
     public void setScene(Scene scene) {
         this.scene = scene;
+    }
 
+    public void numberButtonPressed(ActionEvent actionEvent) {
+        if (label_display.getText().length() < 2) {
+            if (((Button) actionEvent.getSource()).getId().equals("0")) {
+                if (!label_display.getText().equals("")) {
+                    label_display.setText(label_display.getText() + ((Button) actionEvent.getSource()).getId());
+                }
+            } else {
+                label_display.setText(label_display.getText() + ((Button) actionEvent.getSource()).getId());
+            }
+        }
+    }
+
+    public void cancel() {
+        label_display.setText("");
+    }
+
+    public void enter() {
+        int nPerson = Integer.parseInt(label_display.getText());
+        if (nPerson >= 1 && nPerson <= 10) {
+            //TODO: Handle customer panel input
+        }
     }
 }
